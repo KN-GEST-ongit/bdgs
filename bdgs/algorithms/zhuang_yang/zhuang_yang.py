3def88889829aedea9636a7575874d615becb44f
AleksDw <AleksDw2005@gmail.com>
2025-05-20 23:10:28 +0200

    feat: #41 update zhuang_yang.py

--- bdgs/algorithms/zhuang_yang/zhuang_yang.py
+++ bdgs/algorithms/zhuang_yang/zhuang_yang.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import numpy as np
 import pickle
@@ -9,7 +11,8 @@ from bdgs.algorithms.bdgs_algorithm import BaseAlgorithm
 from bdgs.common.crop_image import crop_image
 from bdgs.data.gesture import GESTURE
 from bdgs.data.processing_method import PROCESSING_METHOD
-# from definitions import ROOT_DIR
+from definitions import ROOT_DIR
+
 
 class ZhuangYang(BaseAlgorithm):
     def process_image(self, payload: ZhuangYangPayload,
@@ -21,14 +24,17 @@ class ZhuangYang(BaseAlgorithm):
             image = crop_image(image=image, coords=coords)
 
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # convert into gray
-        image = cv2.resize(image, (80, 80))        # resize to 80x80
+        image = cv2.resize(image, (80, 80))  # resize to 80x80
         image = image.astype(np.float32) / 255.0  # normalize values of pixels from 0-255 to 0-1
 
         return image
 
-    def classify(self, payload: ZhuangYangPayload, custom_model_path = None,
-                 processing_method: PROCESSING_METHOD = PROCESSING_METHOD.DEFAULT) -> GESTURE:
-        model_path = custom_model_path or "../trained_models/zhuang_yang.pkl"
+    def classify(self, payload: ZhuangYangPayload, custom_model_path=None,
+                 processing_method: PROCESSING_METHOD = PROCESSING_METHOD.DEFAULT) -> (GESTURE, int):
+        model_filename = "zhuang_yang.pkl"
+        model_path = os.path.join(custom_model_path, model_filename) if custom_model_path is not None else os.path.join(
+            ROOT_DIR, "trained_models",
+            model_filename)
         with open(model_path, "rb") as f:
             model = pickle.load(f)
 
@@ -47,7 +53,6 @@ class ZhuangYang(BaseAlgorithm):
         pred_label += 1
 
         return GESTURE(pred_label), certainty
-
 
     def learn(self, learning_data: list[ZhuangYangLearningData], target_model_path: str) -> (float, float):
         processed_images = []
@@ -68,11 +73,10 @@ class ZhuangYang(BaseAlgorithm):
 
         processed_images = processed_images.reshape(processed_images.shape[0], -1)
         x_train, x_test, labels_train, labels_test = train_test_split(processed_images, labels, test_size=0.2,
-                                                            random_state=42, stratify=labels)
+                                                                      random_state=42, stratify=labels)
 
         V_train = x_train.T
-        # V_test = x_test[5].reshape(-1, 1)  # one training image
-        V_test = x_test.T             # x training images
+        V_test = x_test.T  # x training images
 
         r = 60
         max_iter = 100
@@ -104,8 +108,8 @@ class ZhuangYang(BaseAlgorithm):
             "phi": phi,
             "sparsity_level": sparsity_level
         }
-
-        with open("../trained_models/zhuang_yang.pkl", "wb") as f:
+        filepath = os.path.join(target_model_path, "zhuang_yang.pkl")
+        with open(filepath, "wb") as f:
             pickle.dump(model, f)
 
         accuracy = float(np.mean(np.array(predictions) == labels_test))
@@ -118,35 +122,36 @@ def image_to_vector(image):
     return image.flatten().reshape(-1, 1)  # flatten to 6400x1 column vector
 
 
-def nmf_update(V, W, H, max_iter=100, epsilon=1e-10):
+def nmf_update(v, w, h, max_iter=100, epsilon=1e-10):
     for _ in range(max_iter):
-        WH = np.dot(W, H) + epsilon
+        WH = np.dot(w, h) + epsilon
 
-        W *= (np.dot(V, H.T)) / (np.dot(WH, H.T) + epsilon) # nominator_W / denominator_W
-        H *= (np.dot(W.T, V)) / (np.dot(W.T, WH) + epsilon) # nominator_H / denominator_H
-        norms = np.linalg.norm(W, axis=0) + epsilon
-        W /= norms
+        w *= (np.dot(v, h.T)) / (np.dot(WH, h.T) + epsilon)  # nominator_W / denominator_W
+        h *= (np.dot(w.T, v)) / (np.dot(w.T, WH) + epsilon)  # nominator_H / denominator_H
+        norms = np.linalg.norm(w, axis=0) + epsilon
+        w /= norms
 
-    return W, H
+    return w, h
 
 
-def omp(A, Y0, sparsity_level=10):
-    d, m, = A.shape
-    residual = Y0.copy()
+def omp(a, y0, sparsity_level=10):
+    d, m, = a.shape
+    residual = y0.copy()
     idx_selected = []
+    theta_ls = []
     theta = np.zeros((m, 1))
 
     for _ in range(sparsity_level):
-        correlations = A.T @ residual
+        correlations = a.T @ residual
         idx = np.argmax(np.abs(correlations))
 
         if idx not in idx_selected:
             idx_selected.append(idx)
 
-        A_selected = A[:, idx_selected]
-        theta_ls, _, _, _ = np.linalg.lstsq(A_selected, Y0, rcond=None)
+        A_selected = a[:, idx_selected]
+        theta_ls, _, _, _ = np.linalg.lstsq(A_selected, y0, rcond=None)
 
-        residual = Y0 - A_selected @ theta_ls
+        residual = y0 - A_selected @ theta_ls
 
         if np.linalg.norm(residual) < 1e-5:
             break
@@ -157,31 +162,32 @@ def omp(A, Y0, sparsity_level=10):
     return theta
 
 
-def classify_by_reconstruction_error(A, Y0, theta, labels_train):
+def classify_by_reconstruction_error(a, y0, theta, labels_train):
     classes = np.unique(labels_train)
     errors = []
 
     for c in classes:
-        mask = (labels_train == c).astype(float).reshape(-1,1)
+        mask = (labels_train == c).astype(float).reshape(-1, 1)
         theta_i = theta * mask
 
-        Y_i = A @ theta_i
+        Y_i = a @ theta_i
 
-        error = np.linalg.norm(Y0 - Y_i)
+        error = np.linalg.norm(y0 - Y_i)
         errors.append(error)
 
     best_class = classes[np.argmin(errors)]
     return best_class, errors
 
-def cs_classify_test_sample(y_0, Y_train, labels_train, phi, sparsity_level=10):
-    A = phi @ Y_train  # (d, m)
-    Y0 = phi @ y_0     # (d, 1)
+
+def cs_classify_test_sample(y_0, y_train, labels_train, phi, sparsity_level=10):
+    A = phi @ y_train  # (d, m)
+    Y0 = phi @ y_0  # (d, 1)
 
     theta = omp(A, Y0, sparsity_level)
     predicted_label, errors = classify_by_reconstruction_error(A, Y0, theta, labels_train)
 
     total_error = np.sum(errors)
     predicted_error = errors[np.argmin(errors)]
-    certainty = 1.0 - predicted_error / (total_error)
+    certainty = 1.0 - predicted_error / total_error
 
     return predicted_label, certainty
